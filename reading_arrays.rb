822174345e4e84bdc8f596d58716d9eb86878f17
Cece Zinny <cwzinny@gmail.com>
2018-07-22 22:56:30 +0000

    Done.

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,24 +9,87 @@ STUDENT_NAMES = [
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[3]
 end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[5]
 end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  STUDENT_NAMES.first
 end
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
+  STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
+  STUDENT_NAMES[0..2]
 end
+
+
+
+
+
+
+
+
+
+
+
+# STUDENT_NAMES = [
+#   "Adele",
+#   "Beyoncé",
+#   "Cardi B",
+#   "Lady Gaga",
+#   "Nicki Minaj",
+#   "Rihanna"
+# ]
+
+
+
+# # puts STUDENT_NAMES[0]
+
+# # puts STUDENT_NAMES[3]
+
+# # puts STUDENT_NAMES[5]
+
+
+# def first_student_by_index
+#   STUDENT_NAMES[0]
+# end
+
+# # puts first_student_by_index
+
+
+# def fourth_student_by_index
+#   puts STUDENT_NAMES[3]
+# end 
+
+
+# def last_student_by_method
+#   STUDENT_NAMES[5]
+# end
+
+# # puts last_student_by_method
+
+# def first_second_and_third_students
+# puts  STUDENT_NAMES[0..2]
+# # shows that it is between 0 and 2, don't need the 1
+# end
+
+# # puts first_second_and_third_students
+
+
+# # puts fourth_student_by_index
+
